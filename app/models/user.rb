a1750eae61f30af757a93c7e23c3d304ae71f6a7
katenorquay <katenorquay@live.com>
2017-05-12 11:07:02 +1200

    devise api working

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  include DeviseTokenAuth::Concerns::User
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  before_create :skip_confirmation!
+end
